6fdda0aa988eb61a373f243a811cf403ed133f2d
iDamina <daminailiya@gmail.com>
2024-07-18 23:47:11 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -16,5 +16,20 @@ def welcome() -> str:
     return jsonify({"message": "Bienvenue"})
 
 
+def users() -> str:
+    """
+    Register a new user
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # regsiter user if user does not exist
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"})
+    except Exception:
+        return jsonify({"message": "email already registered"}), 400
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)

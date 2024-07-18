ece5c86bedcb0e4f31aa4402519ecdf82b591f29
iDamina <daminailiya@gmail.com>
2024-07-18 23:36:06 +0100

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+#!/usr/bin/env python3
+"""
+Basic Flask app
+"""
+
+from flask import Flask, jsonify
+
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Hello, Flask!"
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route("/", methods=["GET"])
+def welcome() -> str:
+    """
+    Return a JSON payload with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
 
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
